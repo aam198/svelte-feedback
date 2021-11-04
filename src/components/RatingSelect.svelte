71175743655d7c44f4ec49d001bd7822bf073e2f
aam198 <aam198@psu.edu>
2021-11-04 15:26:03 -0400

    Fix: RatingSelect, label cfor did not match input id

--- src/components/RatingSelect.svelte
+++ src/components/RatingSelect.svelte
@@ -17,41 +17,50 @@ const onChange = (e) => {
     <input type="radio" id="num1" name="rating" value="1" on:change={onChange} checked={selected === 1} />
     <label for="num1">1</label>
   </li>
+
   <li>
-    <input type="radio" id="num2" name="rating" value="2" on:change={onChange} checked={selected === 2} />
-    <label for="2">2</label>
+    <input type="radio" id="num2" name="rating" value="2" on:change={onChange} checked={selected===2} />
+    <label for="num2">2</label>
   </li>
+
   <li>
     <input type="radio" id="num3" name="rating" value="3" on:change={onChange} checked={selected === 3} />
-    <label for="3">3</label>
+    <label for="num3">3</label>
   </li>
+
   <li>
     <input type="radio" id="num4" name="rating" value="4" on:change={onChange} checked={selected === 4} />
-    <label for="4">4</label>
+    <label for="num4">4</label>
   </li>
+
   <li>
     <input type="radio" id="num5" name="rating" value="5" on:change={onChange} checked={selected === 5} />
-    <label for="5">5</label>
+    <label for="num5">5</label>
   </li>
+
   <li>
     <input type="radio" id="num6" name="rating" value="6" on:change={onChange} checked={selected === 6} />
-    <label for="6">6</label>
+    <label for="num6">6</label>
   </li>
+
   <li>
     <input type="radio" id="num7" name="rating" value="7" on:change={onChange} checked={selected === 7} />
-    <label for="7">7</label>
+    <label for="num7">7</label>
   </li>
+
   <li>
     <input type="radio" id="num8" name="rating" value="8" on:change={onChange} checked={selected === 8} />
-    <label for="8">8</label>
+    <label for="num8">8</label>
   </li>
+
   <li>
     <input type="radio" id="num9" name="rating" value="9" on:change={onChange} checked={selected === 9} />
-    <label for="9">9</label>
+    <label for="num9">9</label>
   </li>
+  
   <li>
     <input type="radio" id="num10" name="rating" value="10" on:change={onChange} checked={selected === 10} />
-    <label for="10">10</label>
+    <label for="num10">10</label>
   </li>
 
 </ul>
